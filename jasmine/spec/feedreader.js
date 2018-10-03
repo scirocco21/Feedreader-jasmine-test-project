77bd40ecb9251bfcf0f6146adc5ab3370ec06cec
Sebastian <sebgertz@gmail.com>
2018-10-03 18:21:51 -0400

    first two tests written and passing

--- jasmine/spec/feedreader.js
+++ jasmine/spec/feedreader.js
@@ -31,29 +31,52 @@ $(function() {
          * in the allFeeds object and ensures it has a URL defined
          * and that the URL is not empty.
          */
+         it('contains only feeds with a URL property', function() {
+           function checkURL(feed) {
+             expect(feed.url).toBeDefined();
+             expect(feed.url).not.toBe("");
+           }
 
+            for (let i = 0; i < allFeeds.length; i++) {
+              checkURL(allFeeds[i])
+            }
+         });
 
         /* TODO: Write a test that loops through each feed
          * in the allFeeds object and ensures it has a name defined
          * and that the name is not empty.
          */
-    });
 
+         it('contains only feeds with a URL property', function() {
+           function checkName(feed) {
+             expect(feed.name).toBeDefined();
+             expect(feed.name).not.toBe("");
+           }
+
+            for (let i = 0; i < allFeeds.length; i++) {
+              checkName(allFeeds[i])
+            }
+         });
+    });
 
     /* TODO: Write a new test suite named "The menu" */
 
+        describe('The menu', function() {
         /* TODO: Write a test that ensures the menu element is
          * hidden by default. You'll have to analyze the HTML and
          * the CSS to determine how we're performing the
          * hiding/showing of the menu element.
          */
-
+          it('is hidden by default', function() {
+            expect($('body')).toHaveClass('menu-hidden')
+            // expect($('.menu-icon-link')).toBeHidden()
+          })
          /* TODO: Write a test that ensures the menu changes
           * visibility when the menu icon is clicked. This test
           * should have two expectations: does the menu display when
           * clicked and does it hide when clicked again.
           */
-
+        })
     /* TODO: Write a new test suite named "Initial Entries" */
 
         /* TODO: Write a test that ensures when the loadFeed
