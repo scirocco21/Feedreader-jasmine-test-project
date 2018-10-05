9f7cffb252d1c778e09c967ddf829720f15908be
Sebastian <sebgertz@gmail.com>
2018-10-05 13:48:30 -0400

    final test defined and passing

--- jasmine/spec/feedreader.js
+++ jasmine/spec/feedreader.js
@@ -14,98 +14,83 @@ $(function() {
     * feeds definitions, the allFeeds variable in our application.
     */
     describe('RSS Feeds', function() {
-        /* This is our first test - it tests to make sure that the
-         * allFeeds variable has been defined and that it is not
-         * empty. Experiment with this before you get started on
-         * the rest of this project. What happens when you change
-         * allFeeds in app.js to be an empty array and refresh the
-         * page?
-         */
-        it('are defined', function() {
-            expect(allFeeds).toBeDefined();
-            expect(allFeeds.length).not.toBe(0);
-        });
+
+      it('are defined', function() {
+      expect(allFeeds).toBeDefined();
+      expect(allFeeds.length).not.toBe(0);
+      });
+
+      it('contains only feeds with a URL property', function() {
+        function checkURL(feed) {
+          expect(feed.url).toBeDefined();
+          expect(feed.url).not.toBe("");
+        };
+
+        for (let i = 0; i < allFeeds.length; i++) {
+          checkURL(allFeeds[i])
+        };
+      });
 
 
-        /* TODO: Write a test that loops through each feed
-         * in the allFeeds object and ensures it has a URL defined
-         * and that the URL is not empty.
-         */
-         it('contains only feeds with a URL property', function() {
-           function checkURL(feed) {
-             expect(feed.url).toBeDefined();
-             expect(feed.url).not.toBe("");
-           }
+      it('contains only feeds with a Name property', function() {
+        function checkName(feed) {
+          expect(feed.name).toBeDefined();
+          expect(feed.name).not.toBe("");
+        };
 
-            for (let i = 0; i < allFeeds.length; i++) {
-              checkURL(allFeeds[i])
-            }
-         });
-
-        /* TODO: Write a test that loops through each feed
-         * in the allFeeds object and ensures it has a name defined
-         * and that the name is not empty.
-         */
-
-         it('contains only feeds with a URL property', function() {
-           function checkName(feed) {
-             expect(feed.name).toBeDefined();
-             expect(feed.name).not.toBe("");
-           }
-
-            for (let i = 0; i < allFeeds.length; i++) {
-              checkName(allFeeds[i])
-            }
-         });
+        for (let i = 0; i < allFeeds.length; i++) {
+          checkName(allFeeds[i])
+        };
+      });
     });
 
-    /* TODO: Write a new test suite named "The menu" */
 
-        describe('The menu', function() {
-        /* TODO: Write a test that ensures the menu element is
-         * hidden by default. You'll have to analyze the HTML and
-         * the CSS to determine how we're performing the
-         * hiding/showing of the menu element.
-         */
-          let body = document.querySelector('body');
+    describe('The menu', function() {
+      let body = document.querySelector('body');
 
-          it('is hidden by default', function() {
-            expect(body.classList.contains('menu-hidden')).toBe(true)
-            // expect($('.menu-icon-link')).toBeHidden()
-          })
+      it('is hidden by default', function() {
+        // body should have class set to menu-hidden by default
+        expect(body.classList.contains('menu-hidden')).toBe(true)
+      });
 
-          it('changes visibility whenever menu icon is clicked', function() {
-            $('.menu-icon-link').trigger('click');
-            expect(body.classList.contains('menu-hidden')).toBe(false)
+      it('changes visibility whenever menu icon is clicked', function() {
+        // simulate both click events to test turning visibility on and off again
+        $('.menu-icon-link').trigger('click');
+        expect(body.classList.contains('menu-hidden')).toBe(false);
+        $('.menu-icon-link').trigger('click');
+        expect(body.classList.contains('menu-hidden')).toBe(true);
+      });
+    });
 
-            $('.menu-icon-link').trigger('click');
-            expect(body.classList.contains('menu-hidden')).toBe(true)
-          })
-         /* TODO: Write a test that ensures the menu changes
-          * visibility when the menu icon is clicked. This test
-          * should have two expectations: does the menu display when
-          * clicked and does it hide when clicked again.
-          */
-        })
-    /* TODO: Write a new test suite named "Initial Entries" */
-      describe('Initial Entries', function() {
-
-        beforeEach(function(done) {
-          $('.feed').html("")
+    describe('Initial Entries', function() {
+      beforeEach(function(done) {
+        $('.feed').html("");
           // done is optional callback for loadFeed, required in this case to stop the 'it' block from running before the asynchronous AJAX call is complete
-          loadFeed(0, done)
-        })
-        /* TODO: Write a test that ensures when the loadFeed
-         * function is called and completes its work, there is at least
-         * a single .entry element within the .feed container.
-         * Remember, loadFeed() is asynchronous so this test will require
-         * the use of Jasmine's beforeEach and asynchronous done() function.
-         */
-         it('should contain at least one entry once loadFeed() has finished execution', function(done) {
-           expect($('.feed').children().length).not.toBe(0);
-           // without invoking done() here, Jasmine complains: Async callback was not invoked within timeout specified by jasmine.DEFAULT_TIMEOUT_INTERVAL.
-           // Cf. docs: https://jasmine.github.io/2.3/introduction.html#section-Asynchronous_Support
-           done();
-         });
+        loadFeed(0, done)
+      });
+
+      it('should contain at least one entry once loadFeed() has finished execution', function(done) {
+        expect($('.feed').children().length).not.toBe(0);
+        // without invoking done() here, Jasmine complains: Async callback was not invoked within timeout specified by jasmine.DEFAULT_TIMEOUT_INTERVAL.
+        // Cf. docs: https://jasmine.github.io/2.3/introduction.html#section-Asynchronous_Support
+         done();
+       });
      });
+
+    describe('New Feed Selection', function() {
+      $('.feed').html("");
+      let firstFeedEntries;
+      // load both feeds before running the specs; store entries of first feed before loading the second one
+      beforeEach(function(done) {
+        loadFeed(0)
+        firstFeedEntries = $('.feed').children();
+        loadFeed(1, done)
+      });
+
+      it('should generate new content when a different feed is loaded', function (done) {
+        // if both feeds have different content, expect their first entries to have different urls
+        expect(firstFeedEntries[0].href).not.toEqual($('.feed').children()[0].href);
+        done();
+      });
+    });
 }());
